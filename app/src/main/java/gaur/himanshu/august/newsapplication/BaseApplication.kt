08da8f9001336cda4fa845e8b266033e70b96a08
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2021-01-03 13:31:29 +0530

    Setup Dagger Hilt

---
+++ app/src/main/java/gaur/himanshu/august/newsapplication/BaseApplication.kt
@@ -0,0 +1,7 @@
+package gaur.himanshu.august.newsapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application()
---
+++ app/src/main/java/gaur/himanshu/august/newsapplication/di/DiModules.kt
@@ -0,0 +1,9 @@
+package gaur.himanshu.august.newsapplication.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object DiModules
