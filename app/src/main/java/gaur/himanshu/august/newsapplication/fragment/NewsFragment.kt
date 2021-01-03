ac5597b48af0dcf2ea410b89af829aa0849649ce
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2021-01-03 13:09:57 +0530

    Initial commit

---
+++ app/src/main/java/gaur/himanshu/august/newsapplication/fragment/NewsFragment.kt
@@ -0,0 +1,26 @@
+package gaur.himanshu.august.newsapplication.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import gaur.himanshu.august.newsapplication.R
+
+
+class NewsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+}
